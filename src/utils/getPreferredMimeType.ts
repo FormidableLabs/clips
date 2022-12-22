5c43b175b12083ff434ed7554a9cedda4a506d8c
Grant Sander <gksander93@gmail.com>
2022-12-21 20:50:22 -0600

    Update getPreferredMimeType.ts

--- src/utils/getPreferredMimeType.ts
+++ src/utils/getPreferredMimeType.ts
@@ -2,7 +2,6 @@ export const getPreferredMimeType = () => {
   return MIME_TYPES.find(mimeType => MediaRecorder.isTypeSupported(mimeType.mimeType));
 };
 
-
 const MEDIA_TYPES = ["video"];
 const FILE_EXTENSIONS = ["mp4", "webm", "ogg", "x-matroska"];
 const CODECS = [
@@ -19,8 +18,7 @@ const CODECS = [
   "opus"
 ];
 
-
-const MIME_TYPES = [...new Set(
+const MIME_TYPES: { mimeType: string; ext: string }[] = [...new Set(
   FILE_EXTENSIONS.flatMap(ext => CODECS.flatMap(codec => MEDIA_TYPES.flatMap(mediaType => [
     { mimeType: `${mediaType}/${ext};codecs:${codec}`, ext },
     { mimeType: `${mediaType}/${ext};codecs=${codec}`, ext },
